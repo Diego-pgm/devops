6bbce990d3e557a6d1c8c5be69bff51084b5ec34
Diego Pérez Gramond Muñoz <diego.pegramm@gmail.com>
2025-01-22 20:14:00 -0600

    Implemented copy files created into a server

--- jenkins/backup-jenkins.sh
+++ jenkins/backup-jenkins.sh
@@ -8,13 +8,13 @@ gzip /opt/jenkins-bak.tar
 
 echo 'Backup done.'
 
-read -p "Copy files to backup server?" USER_INPUT
+read -p "Copy files to backup server?: " USER_INPUT
 
 if [[ "$USER_INPUT" == "y" || "$USER_INPUT" == "yes" ]];
   then 
     read -p "Insert server username: " USER
     read -p "Insert server host/ip: " HOST
-    read -p "Insert directory path: " PATH
-    scp /opt/jenkins-bak.tar $USER@$HOST:$PATH
+    read -p "Insert directory path: " SPATH
+    scp /opt/jenkins-bak.tar.gz $USER@$HOST:$SPATH
     echo "Server backup done."
 fi
